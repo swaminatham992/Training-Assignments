910f8bfb48f4f76e9c73fa939e7cabae61cc6972
swaminatham992 <103921685+swaminatham992@users.noreply.github.com>
2022-06-12 01:21:31 +0530

    Add files via upload

---
+++ MVCAssignment2/Migrations/202206111925434_Initial-Create.cs
@@ -0,0 +1,27 @@
+﻿namespace MVCAssignment2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TblMovie",
+                c => new
+                    {
+                        MovieId = c.Int(nullable: false, identity: true),
+                        MovieName = c.String(),
+                        ReleaseDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.MovieId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TblMovie");
+        }
+    }
+}
