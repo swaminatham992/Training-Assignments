03d4c091879b0869cc42fc6f412f2ceb08994107
swaminatham992 <103921685+swaminatham992@users.noreply.github.com>
2022-06-16 18:38:28 +0530

    Add files via upload

---
+++ HTML Codebase/HTML.cs
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title> Registration Form</title>
+    
+   
+</head>
+<body>
+    <body>
+        <h1>Create an Account</h1>
+        <form>
+            Email <br />
+            <input type="text" name="email" /><br />
+            Password <br />
+            <input type="password" name="psw" /><br />
+            Age <br /> &nbsp;&nbsp;<select name="Age">
+                <option value="age">select Age</option>
+                <option value="age1">18</option>
+                <option value="age2">19</option>
+                <option value="age3">24</option>
+                <option value="age4">25</option>
+                <option value="age5">29</option>
+            </select><br><br>
+            Tell us a little about yourself <br />
+<textarea rows="4" cols="35"></textarea><br><br>
+            <input type="checkbox" name="id1" value="ac" /> I agree to the <a href="#">terms and conditions</a><br />
+            <button type="submit">signup</button>
+        </form>
+
+
+
+    </body>
+
+</body>
+</html>
