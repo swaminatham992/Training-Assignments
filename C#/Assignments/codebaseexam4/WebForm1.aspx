816bca7a1006709e44ce440fd4095b10c8d356bf
swaminatham992 <103921685+swaminatham992@users.noreply.github.com>
2022-06-16 18:41:00 +0530

    Add files via upload

---
+++ C#/Assignments/codebaseexam4/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/CodeBased.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="codebaseexam4.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <center>
+         <form id="form1" runat="server">
+        
+            <p>Select Product From the below List:</p>
+            <asp:DropDownList ID="ProductDropDown" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ProductDropDown_SelectedIndexChanged">
+                <asp:ListItem Value="">Select Product</asp:ListItem>
+                <asp:ListItem>Camera</asp:ListItem>
+                <asp:ListItem>Headphone</asp:ListItem>
+                <asp:ListItem>Mobile</asp:ListItem>
+                <asp:ListItem>Smartwatch</asp:ListItem>
+            </asp:DropDownList>
+
+              <asp:Button ID="Price" runat="server" Text="Get Price" onClick="Button1_Click"/>
+           
+         </form>
+         <br />
+            <br />
+             <asp:Label ID="Message" runat="server" Text="Select one product" > </asp:Label>
+            <br />
+            <br /> 
+            <asp:Image ID="ProductImage" runat="server" Visible="false" Height="260px" Width="299px" />
+            <br />
+            <br />
+            <asp:Label ID="ProductPrice" runat="server" Text="Label"  Visible="false"> </asp:Label>
+           
+    </center>
+        
+</asp:Content>
---
+++ C#/Assignments/codebaseexam4/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace codebaseexam4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private List<float> prices = new List<float>();
+        private List<String> products = new List<String>();
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            products.Add("Camera");
+            products.Add("Headphone");
+            products.Add("Mobile");
+            products.Add("Smartwatch");
+            prices.Add((float)30000.00);
+            prices.Add((float)25000.00);
+            prices.Add((float)15000.00);
+            prices.Add((float)40000.00);
+
+        }
+        protected void ProductDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ProductDropDown.SelectedItem.Value.Equals(""))
+            {
+                ProductImage.Visible = false;
+                ProductPrice.Visible = false;
+                Price.Visible = false;
+                Message.Text = "Select one product";
+            }
+            else
+            {
+                Message.Text = "You Selected: " + ProductDropDown.SelectedItem.Value;
+                ProductImage.ImageUrl = "~/Content/" + ProductDropDown.SelectedItem.Value + ".jpg";
+                ProductImage.Visible = true;
+                Price.Visible = true;
+
+            }
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            ProductPrice.Visible = true;
+            ProductPrice.Text = "Price Rs. " + prices.ElementAt(products.IndexOf(ProductDropDown.SelectedItem.Value));
+        }
+    }
+}
